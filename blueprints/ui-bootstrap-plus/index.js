e014e752bb19a95773b5fef07dc5def2c928d309
Ken Snyder <ken@ken.net>
2016-05-14 18:20:38 +0100

    moved to "addons" versus "npm" where appropriate

--- blueprints/ui-bootstrap-plus/index.js
+++ blueprints/ui-bootstrap-plus/index.js
@@ -1,4 +1,5 @@
 /*jshint node:true*/
+var RSVP = require('rsvp');
 module.exports = {
   description: 'ui-bootstrap with complementary packages included',
 
@@ -14,15 +15,19 @@ module.exports = {
   },
 
   afterInstall: function() {
-    return this.addPackagesToProject([
-      {name: 'ui-bootstrap', target: '*'},
-      {name: 'ember-tether', target: '^0.3.1'},
+    var npm = this.addPackagesToProject([
       {name: 'ui-button', target: '^2.0.0-beta.8'},
-      {name: 'ui-icon', target: '*'},
       // {name: 'ui-tooltip', target: '*'},
       // {name: 'ui-dropdown', target: '*'},
       {name: 'ember-cli-flash', target: '^1.3.15'},
       {name: 'ember-modal-dialog', target: '^0.8.4'},
+    ]);
+
+    return RSVP.Promise.all([
+      npm,
+      this.addAddonToProject('ui-bootstrap'),
+      this.addAddonToProject('ui-icon'),
+      this.addAddonToProject('ember-tether'),
     ]);
 	}
 
