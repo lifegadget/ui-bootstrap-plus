5f480b715f4a01d73f9fbeab10f08f6cc441e1c5
Ken Snyder <ken@ken.net>
2016-05-14 16:50:55 +0100

    minor adjustments

---
+++ blueprints/ui-bootstrap-plus/index.js
@@ -0,0 +1,30 @@
+/*jshint node:true*/
+module.exports = {
+  description: 'ui-bootstrap with complementary packages included',
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  normalizeEntityName: function() {
+
+  },
+
+  afterInstall: function() {
+    return this.addPackagesToProject([
+      {name: 'ui-bootstrap', target: '*'},
+      {name: 'ember-tether', target: '0.3.1'},
+      {name: 'ui-button', target: '*'},
+      {name: 'ui-icon', target: '*'},
+      // {name: 'ui-tooltip', target: '*'},
+      // {name: 'ui-dropdown', target: '*'},
+      {name: 'ember-cli-flash', target: '1.13.15'},
+      {name: 'ember-modal-dialog', target: '0.8.4'},
+    ]);
+
+	}
+
+};
